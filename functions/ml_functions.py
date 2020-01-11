c892ad98d1778d9bea22f38977e6b03cda487d62
Ahmed Ben Romdhane <ahmedbenromdhane@MacBook-Air-de-Ahmed.local>
2020-01-11 11:01:56 +0100

    Added ML functions descriptions

--- functions/ml_functions.py
+++ functions/ml_functions.py
@@ -11,39 +11,55 @@ from sklearn.metrics import accuracy_score, f1_score
 
 
 def Rf_classification(X_train, y_train, X_test, y_test, tscv, score = 'accuracy'):
-    """
-    Random forest classification
+    '''
+    Optimize a Random Forest classifier by cross-validation 
+    (according to the selected metrics), then build the optimized model 
+    and test its accuracy on the test set.
     
     Parameters
-    ----------
+    -----------
+    X_train, y_train, X_test, y_test: pandas.DataFrame
+         Train and test sets
+    
+    tscv: TimeSeriesSplit()
+    
+    score: metrics
+        metrics
     
     Returns
     --------
-    """
-   
-    seed = 1
+    y_pred: array
+        predictions of the model
         
-    # Define a random classifier pipeline
+    pipeline: sklearn.pipeline.Pipeline 
+        Contains the trained model
+    '''
+        
+    # Define a random forest classifier pipeline
     estimators = []
     estimators.append(('rf_clf', RandomForestClassifier()))
     pipeline = Pipeline(estimators)
 
-    # Fixed parameters
+    # Fixed parameters 
+    seed = 1
     score = score
+    
+    # Hyper parameter settings and cross validation schema
     tuning_param_range = [int(i) for i in np.linspace(10, 100, 10)]
     tuning_param = 'rf_clf__n_estimators'
     cv_schema = tscv
 
 
-    # Tune hyper parameter using validation curve
+    # Compute training and test scores for varying parameter values 
     train_scores_val, cv_scores_val = validation_curve(
         pipeline, X_train, y_train, param_name = tuning_param, param_range = tuning_param_range,
         cv = cv_schema, scoring = score, n_jobs = -1)
 
-    # Obtain the best value of the hyper parameter, with the best accuracy score
+    # Obtain the best value for the hyper parameter, with the best accuracy score
     best_param_val_rf = tuning_param_range[np.argmax(np.mean(cv_scores_val, axis=1))]
     best_rf_acc = max(np.mean(cv_scores_val, axis=1))
     
+    # Build the model with the best parameter
     pipeline.set_params(rf_clf__n_estimators = best_param_val_rf)
     pipeline.fit(X_train,y_train)
     y_pred = pipeline.predict(X_test)
@@ -54,15 +70,31 @@ def Rf_classification(X_train, y_train, X_test, y_test, tscv, score = 'accuracy'
 
 
 def GBT_classification(X_train, y_train, X_test, y_test, tscv, ratio = None, score = 'f1_weighted'):
-    """
-    Gradient boosted trees classification
+    '''
+    Optimize an XGBoost classifier by cross-validation (according to the selected metrics), 
+    then build the optimized model and test its accuracy on the test set.
     
     Parameters
-    ----------
+    -----------
+    X_train, y_train, X_test, y_test: pandas.DataFrame
+        Train and test sets
+    
+    tscv: TimeSeriesSplit()
+    
+    ratio: float
+       total number of negative class
+       instances over positive class
+    
+    score: metrics
+        metrics
     
     Returns
     --------
-    """
+    xgb_pred: array
+        predictions of the model
+    
+    optimal_xgb: trained optimal model
+    '''
  
     estimators = []
     if ratio :
@@ -77,24 +109,25 @@ def GBT_classification(X_train, y_train, X_test, y_test, tscv, ratio = None, sco
     n_splits = 3
     seed=0
     
-    # CV schema
+    # Cross validation schema
     cv_schema = tscv
 
-    # Tune model against a single hyper parameter
+    # hyper parameter and range of optimization
     tuning_param = 'XGB_clf__n_estimators'
     tuning_param_range = [int(i) for i in np.linspace(10.0, 30.0, 5)]
 
-    # Tune hyper parameter using validation curve
+    # hyper parameter tuning
     train_scores_val, cv_scores_val = validation_curve(
         gb_pipe, X_train, y_train, param_name = tuning_param, param_range = tuning_param_range,
         cv = cv_schema, scoring = score, n_jobs = -1)
 
-    # Obtain the best value of the hyper parameter
+    # Obtain the best value for the hyper parameter and the accuracy score
     best_param_val_GBT = tuning_param_range[np.argmax(np.mean(cv_scores_val, axis=1))]
     best_xgb_acc = max(np.mean(cv_scores_val, axis=1))
     #print(best_param_val_GBT)
     #print(best_xgb_acc)
     
+    # Build the model
     if ratio :
         optimal_xgb =  XGBClassifier(n_estimators=best_param_val_GBT, scale_pos_weight = ratio)
     else :
@@ -104,18 +137,24 @@ def GBT_classification(X_train, y_train, X_test, y_test, tscv, ratio = None, sco
     xgb_pred = optimal_xgb.predict(X_test)
     acc_xgb = accuracy_score(y_test, xgb_pred)
     f1_xgb = f1_score(y_test, xgb_pred, average = "weighted")
-    print("Gradient Boosted trees model accuracy score : " + str(acc_xgb))
-    print("Gradient Boosted trees model F1 score : " + str(f1_xgb))
+    print("XGB model accuracy score : " + str(acc_xgb))
+    print("XGB model F1 score : " + str(f1_xgb))
     
     return xgb_pred, optimal_xgb
 
 
 def plot_confusion_matrix(y_true, y_pred, classes, title=None, cmap=plt.cm.Blues, figsize=(6,6)):
     """
-    This function prints and plots the confusion matrix.
+    compute and plot the confusion matrix.
     
     Parameters
     ----------
+    y_true, y_pred: arrays
+        test labels and predictions, 
+        respectively
+    
+    classes: list of strings
+        labels for of the class
     
     Returns
     --------
@@ -129,10 +168,9 @@ def plot_confusion_matrix(y_true, y_pred, classes, title=None, cmap=plt.cm.Blues
     im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
     ax.figure.colorbar(im, ax=ax)
     
-    # Show ticks
+    # Fix ticks and label them with the respective list entries
     ax.set(xticks=np.arange(cm.shape[1]),
            yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
            xticklabels=classes, yticklabels=classes,
            title=title,
            ylabel='True label',
